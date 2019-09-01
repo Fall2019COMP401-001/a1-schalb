299d71a179e1b38e459ca2bf288a119713f2f15b
alber <alber@DESKTOP-HTF5KQ3>
2019-09-01 15:03:30 -0400

    Completed A1Novice.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,22 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
+		int customers = scan.nextInt();
+		for (int i = 0; i < customers; i++) {
+			String first = scan.next();
+			String last = scan.next();
+			double total = 0;
+			int items = scan.nextInt();
+			for (int u = 0; u < items; u++) {
+				double a = scan.nextDouble();
+				String food = scan.next();
+				double b = scan.nextDouble();
+				total += a * b;
+			}
+			System.out.println(first.charAt(0) + ". " + last + ": " + total);
+		}
+		scan.close();
 		// Your code follows here.
-		
+				
 	}
 }
