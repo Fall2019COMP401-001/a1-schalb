ead7e33cc5440101f067b9030aa3a44c519a1543
alber <alber@DESKTOP-HTF5KQ3>
2019-09-01 18:53:29 -0400

    Revision A1 Novice with 2 decimal places.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,11 +7,12 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		
 		int customers = scan.nextInt();
 		for (int i = 0; i < customers; i++) {
 			String first = scan.next();
 			String last = scan.next();
-			double total = 0;
+			double total = 0.00;
 			int items = scan.nextInt();
 			for (int u = 0; u < items; u++) {
 				double a = scan.nextDouble();
@@ -19,10 +20,10 @@ public class A1Novice {
 				double b = scan.nextDouble();
 				total += a * b;
 			}
-			System.out.println(first.charAt(0) + ". " + last + ": " + total);
+			String totalS = String.format("%.2f", total);
+			System.out.println(first.charAt(0) + ". " + last + ": " + totalS);
 		}
 		scan.close();
-		// Your code follows here.
-				
+		
 	}
 }
